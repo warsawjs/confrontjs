051032976e5470fe9f89ef89d69814c03dbf17fc
Konrad Dziekonski <jaseveen@gmail.com>
2021-07-27 21:38:46 +0200

    add avital-tzubeli profile and photo

---
+++ src/data/speakers/avital-tzubeli.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "Avital Tzubeli",
+  position: "Regional Advocate @Vonage | Technical Storytelling Coach",
+  company: "Vonage",
+  country: "Israel",
+  bio: {
+    en: "From a young age, Avital was fascinated by the world of computers and solving problems with them. She followed a path of backend development, storytelling, and a little graphic design, before discovering the world of DevRel. Today Avital is the Regional Developer Advocate at Vonage TLV, where she combines her passions for clean code and encouraging developers to tell good stories.",
+  },
+  avatar_url: "/speakers/avital-tzubeli.jpg",
+  contact: {
+    twitter_url: "https://twitter.com/avitaltzubeli",
+    linkedin_url: "https://www.linkedin.com/in/tzubeli/",
+  },
+};
