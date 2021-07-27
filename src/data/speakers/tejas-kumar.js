1daaf1109a2f38cf07eb9944a74633522c335430
Konrad Dziekonski <jaseveen@gmail.com>
2021-07-27 22:37:15 +0200

    add Tejas Kumar to data/speakers

---
+++ src/data/speakers/tejas-kumar.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "Tejas Kumar",
+  position: "Senior Engineer @Spotify",
+  company: "Spotify",
+  country: "Germany",
+  bio: {
+    en: "Athlete; Senior Engineer @Spotify Award-winning web developer, speaks at conferences and co. about JavaScript and the web.",
+  },
+  avatar_url: "/speakers/tejas-kumar.jpg",
+  contact: {
+    twitter_url: "https://twitter.com/tejaskumar_",
+    github_url: "https://github.com/tejasq",
+  },
+};
