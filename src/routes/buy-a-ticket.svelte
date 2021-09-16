21461cf1c942afda7534206733dd6882dc7eda6f
Konrad Dziekonski <jaseveen@gmail.com>
2021-09-17 00:24:14 +0200

    add buy-a-ticker route

---
+++ src/routes/buy-a-ticket.svelte
@@ -0,0 +1,14 @@
+<script>
+  import TicketSelection from "../components/sections/TicketSelection.svelte";
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+  <title>Buy a ticket • ConFrontJS 2021 • Front-end Conference in Warsaw</title>
+</svelte:head>
+
+<div class="">
+  <TicketSelection />
+</div>
