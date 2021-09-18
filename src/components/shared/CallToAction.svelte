8c115f14bbccfbd07040d805861c4f11ae25ac27
Konrad Dziekonski <jaseveen@gmail.com>
2021-09-18 20:27:30 +0200

    change BuyTicketsButton to CallToAction and move it from sections to shared

---
+++ src/components/shared/CallToAction.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let href;
+</script>
+
+<style>
+    .buy-tickets-container {
+        width: 100%;
+        text-align: center;
+        margin: 50px 0;
+    }
+
+</style>
+
+<div class="buy-tickets-container">
+
+    <a
+        class="button-primary"
+        href={href}
+    >
+    <slot />
+    </a>
+
+</div>
