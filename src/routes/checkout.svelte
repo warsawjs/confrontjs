88fa69f574b903b9263f89665a02d35db7fcbae8
Konrad Dziekonski <jaseveen@gmail.com>
2021-10-02 19:11:58 +0200

    update checkout with stripe payment

--- src/routes/checkout.svelte
+++ src/routes/checkout.svelte
@@ -1,9 +1,14 @@
 <script>
-  import { onDestroy } from 'svelte'
+  import { onDestroy } from 'svelte';
+  import { loadStripe } from '@stripe/stripe-js';
+  import * as EmailValidator from 'email-validator';
   import CheckoutListItem from '../components/shared/CheckoutListItem.svelte';
   import ticketStore from '../components/stores/ticket-store';
 
+
   let ticketsInStore = []
+  let userEmail = '';
+  let emailError = '';
 
   const unsubscribe = ticketStore.subscribe(store => {
     ticketsInStore = store
@@ -33,6 +38,32 @@
       ticketStore.removeTicket(selectedItem.title)
     }
  }
+
+ function createPaymentData() {
+   const details = ticketsInStore.map(({id, quantity}) => ({ price:id, quantity }))
+   return details
+ }
+
+ async function submit() {
+    const stripe = await loadStripe('pk_test_51JfX15EjkyMmX0J5hhdGQbYpaLSXrZ5SvjpGqCvSlGUFbmOy3r82qWd2Eldy6yIqNSpvouXVWmrxcrrwR0Y9xfta00aHj6Djhs');
+
+    const lineItems = createPaymentData()
+    if (!userEmail) {
+      emailError = 'Please provide valid email address'
+    }
+    else if (!EmailValidator.validate(userEmail)) {
+      emailError = 'Incorrect email'
+    } else {
+      emailError = ''
+      stripe.redirectToCheckout({ 
+        lineItems, 
+        mode: 'payment', 
+        customerEmail: userEmail, 
+        successUrl: 'https://confrontjs.netlify.app/order-success', 
+        cancelUrl: 'https://confrontjs.netlify.app/order-cancel' 
+      }).then(res => console.log(res)).catch(err => console.log(err))
+    }
+  }
 </script>
 
 <style>
@@ -57,6 +88,8 @@
   }
   .ticket-list ul {
     padding-inline-start: 0;
+    max-height: 370px;
+    overflow-y: auto;
   }
   .cart-icon {
     height: 30vh;
@@ -64,18 +97,45 @@
   }
   .button-primary {
     background-color: var(--mix-color);
+    border: none;
     position: relative;
     transform: translateX(15px);
-    margin-top: 20px
   }
-  .payment {
-    padding-top: 50px;
+
+  .margin-top {
+    margin-top: 20px;
+  }
+
+  .details-confirm {
+    display: flex;
+    align-items: center;
+  }
+
+  .email-input {
+    height: 35px;
+    padding: 7px;
+    border-radius: 7px;
+    border: 1px solid var(--mix-color);
+    transform: translateX(5px);
+  }
+  .email-error {
+    color: red;
+    font-size: 10px;
   }
 
   @media(max-width: 800px) {
     .checkout-wrapper {
       flex-direction: column;
     }
+
+    .details-confirm{
+      flex-direction: column;
+    }
+
+    .button-primary {
+    margin-top: 10px;
+    transform: translateX(5px);
+  }
   }
 </style>
 
@@ -99,20 +159,28 @@
           />
         {/each}
       </ul>
+      <div class="details-confirm">
+        <label for="payment-email">Email</label>
+        <input type="email" id="payment-email" bind:value="{userEmail}" class="email-input"/>
+        <button
+        class="button-primary"
+        on:click="{submit}"
+        >Confirm and pay
+      </button>
+      </div>
+      <div class="email-error">{emailError}</div>
     </div>
     {:else}
       <h3>The cart is empty</h3>
       <img class="cart-icon" src="/shopping-cart-svgrepo-com.svg"  alt="empty-cart-icon"/>
       <a
       class="button-primary"
+      class:margin-top={!$ticketStore.length}
       data-content="Get tickets"
       href="/buy-a-ticket"
       >Get tickets</a>
     {/if}
   </article>
-  {#if $ticketStore.length}
-  <article class="payment"><h2>Payment center</h2></article>
-  {/if}
  </section>
 
 
