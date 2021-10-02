b9030b6c2ae90eb6c761c62c8853b412a20706c5
Konrad Dziekonski <jaseveen@gmail.com>
2021-10-02 19:11:01 +0200

    add order success and cancel subpages scaffold

---
+++ src/routes/order-cancel.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<style>
+  .vh {
+    height: 50vh;
+  }
+</style>
+
+<svelte:head>
+  <title>Order cancel • ConfrontJS 2021 • Front-end Conference in Warsaw</title>
+</svelte:head>
+
+<div class="vh">
+  <h1>nie hura nie kupiłeś!</h1>
+</div>
---
+++ src/routes/order-success.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<style>
+  .vh {
+    height: 50vh;
+    display: flex;
+    justify-content: center;
+  }
+</style>
+
+<svelte:head>
+  <title>Order success • ConfrontJS 2021 • Front-end Conference in Warsaw</title>
+</svelte:head>
+
+<div class="vh">
+  <h1>hura kupiłeś!</h1>
+</div>
