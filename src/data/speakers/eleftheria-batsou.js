b0517dce8a1cd868862e3d966314b37eaf332065
Konrad Dziekonski <jaseveen@gmail.com>
2021-11-16 18:10:24 +0100

    update Elefterias company info

--- src/data/speakers/eleftheria-batsou.js
+++ src/data/speakers/eleftheria-batsou.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "Eleftheria Batsou",
   position: "Community Manager",
-  company: "MKI Hellas",
+  company: "Hashnode",
   country: "Greece",
   bio: {
     en: `Eleftheria is a Community Manager at Hashnode by day and a developer/UX researcher by night. Her studies include a major in Informatics and Telecommunications of engineering and a master's in Graphic Arts and Multimedia. She has created tech courses for several platforms such as Packt, Udemy, and Skillshare and she likes to produce motivational talks and how-to videos on her YouTube channel. She is self-motivated and working hard to make this world a little bit better every day.
