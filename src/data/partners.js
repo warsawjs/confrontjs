540987f7be69d5a6e74dd960417645186b7a46ce
Konrad Dziekonski <jaseveen@gmail.com>
2021-11-18 21:38:54 +0100

    add partners

--- src/data/partners.js
+++ src/data/partners.js
@@ -1,3 +1,13 @@
 module.exports = [
-  // require('./partners/warsawjs'),
+  require("./partners/warsawjs"),
+  require("./partners/deno-warsaw"),
+  require("./partners/k-mag"),
+  require("./partners/devenv"),
+  require("./partners/devszczepaniak"),
+  require("./partners/highlab"),
+  require("./partners/justjoin-it"),
+  require("./partners/mzalewski"),
+  require("./partners/pawel-ochota"),
+  require("./partners/typeofweb"),
+  require("./partners/sowa-programuje"),
 ];
