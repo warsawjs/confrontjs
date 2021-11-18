04829bb3367319bc4ae659ff56dfcbeac2f35497
Konrad Dziekonski <jaseveen@gmail.com>
2021-11-18 21:38:38 +0100

    add K-Mag, update sowa programuje

---
+++ src/data/partners/k-mag.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "K-Mag",
+  url: "https://k-mag.pl/",
+  logo_url:
+    "https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/sponsors/logo-kmag.png",
+};
--- src/data/partners/sowa-programuje.js
+++ src/data/partners/sowa-programuje.js
@@ -1,5 +1,6 @@
 module.exports = {
-    name: 'Sowa Programuje',
-    url: 'http://sowaprogramuje.pl/',
-    logo_url: ''
+  name: "Sowa Programuje",
+  url: "http://sowaprogramuje.pl/",
+  logo_url:
+    "https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/sponsors/logo-sowa-programuje.svg",
 };
