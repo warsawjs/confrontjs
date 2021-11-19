7b77b5568e42bf15136780cf1bc8c0b77d7b63bd
Konrad Dziekonski <jaseveen@gmail.com>
2021-11-19 10:14:23 +0100

    update K-Mag logo and partners layout

--- src/data/partners/k-mag.js
+++ src/data/partners/k-mag.js
@@ -2,5 +2,5 @@ module.exports = {
   name: "K-Mag",
   url: "https://k-mag.pl/",
   logo_url:
-    "https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/sponsors/logo-kmag.png",
+    "https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/sponsors/logo-kmag.svg",
 };
