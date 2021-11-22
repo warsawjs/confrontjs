cbdf257e148172598a51428b0b24039f52b132cc
Konrad Dziekonski <jaseveen@gmail.com>
2021-11-22 15:00:02 +0100

    add nowoczesny frontend logo

--- src/data/partners/nowoczesny-frontend.js
+++ src/data/partners/nowoczesny-frontend.js
@@ -1,5 +1,6 @@
 module.exports = {
-    name: 'Nowoczesny Frontend',
-    url: 'https://nowoczesny-frontend.pl/',
-    logo_url: ''
+  name: "Nowoczesny Frontend",
+  url: "https://nowoczesny-frontend.pl/",
+  logo_url:
+    "https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/sponsors/logo-nowoczesny-frontend.png",
 };
