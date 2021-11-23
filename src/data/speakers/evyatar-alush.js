50de2b85bb16401a3c26d3ffa41603b8696f1d63
Konrad Dziekonski <jaseveen@gmail.com>
2021-11-23 14:08:35 +0100

    update Evyatars photo

--- src/data/speakers/evyatar-alush.js
+++ src/data/speakers/evyatar-alush.js
@@ -6,7 +6,8 @@ module.exports = {
   bio: {
     en: `Front End Engineer at Facebook and author of Vest validations framework. Open source enthusiast and passionate about dev ergonomics and API design.`,
   },
-  avatar_url: "/speakers/evyatar-alush.jpg",
+  avatar_url:
+    "https://s3.eu-central-1.amazonaws.com/www.warsawjs.com/static/images/people/evyatar-alush.jpg",
   contact: {
     twitter_url: "https://twitter.com/evyataral",
     github_url: "https://github.com/ealush",
