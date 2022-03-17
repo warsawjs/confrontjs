77f11e4a2eb777a548e93bdd99da259ad79e83f2
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-17 23:41:47 +0100

    feat(speakers): add new speakers - Zaiste, Orr Gottlieb, Maciej Kaczanowski

---
+++ src/data/speakers/jakub-neander.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "Jakub Zaiste Neander",
+  position: "",
+  company: "",
+  country: "Poland",
+  bio: {
+    en: "",
+  },
+  avatar_url: "/speakers/jakub-neander.jpeg",
+  contact: {
+    twitter_url: "https://twitter.com/zaiste",
+    linkedin_url: "",
+  },
+};
---
+++ src/data/speakers/orr-gottlieb.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "Orr Gottlieb",
+  position: "Tech Lead",
+  company: "Monday.com",
+  country: "",
+  bio: {
+    en: ``,
+  },
+  avatar_url: "/speakers/orr-gottlieb.png",
+  contact: {
+    twitter_url: "",
+    linkedin_url: "",
+  },
+};
