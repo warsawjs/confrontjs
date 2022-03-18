c4a1eb2db02ddbfb4b5c3ce99a1cf9766fe408c1
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-18 11:42:03 +0100

    refactor(MOC): add Barrys bio

--- src/data/speakers/barry-solone.js
+++ src/data/speakers/barry-solone.js
@@ -4,7 +4,9 @@ module.exports = {
   company: "",
   country: "",
   bio: {
-    en: "",
+    en: `
+Barry Solone
+Multi-faceted artist, entertainer and  presenter.  Voice over actor who can be heard in his monthly podcast “RADIO IT”  on several social media platforms. Communications advisor for Xfaang , Has presided as Master of Ceremonies for several WJS events and will return this year as M.O.C. for ConfrontJS 2022`,
   },
   avatar_url: "/speakers/barry-solone.jpg",
   contact: {
