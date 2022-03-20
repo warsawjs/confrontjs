111fe590d3de23f65c709e3f1c2b8813bf872006
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-20 20:03:53 +0100

    feat(speaker): add Chen Salomon profile without photo

---
+++ src/data/speakers/chen-salomon.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "Chen Salomon",
+  position: "Development Group Mnager",
+  company: "Monday.com",
+  country: "Israel",
+  bio: {
+    en: `Chen is a development group manager at monday.com. In his previous role, he was VP R&D at Playbuzz/ex.co. In the last few years, Chen has managed teams and groups of several sizes, including remote teams. He has over a decade of experience in engineering, mainly on the server-side, and he specializes in high-scale web environments, cloud, and microservices architectures.`,
+  },
+  avatar_url: "/speakers/",
+  contact: {
+    twitter_url: "",
+    linkedin_url: "",
+  },
+};
