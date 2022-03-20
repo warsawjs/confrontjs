6ab8718182e3da945892876c8acec903d363cf28
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-20 14:21:04 +0100

    refactor(speaker):update Orr Gotlieb bio

--- src/data/speakers/orr-gottlieb.js
+++ src/data/speakers/orr-gottlieb.js
@@ -4,7 +4,7 @@ module.exports = {
   company: "Monday.com",
   country: "Israel",
   bio: {
-    en: ``,
+    en: `Orr is a frontend tech lead at monday.com. In his previous role he was a team lead at Duda.co. in the last few years. Orr main passion in monday.com is making other developers life simpler and more efficient. He has almost a decade of experience in frontend engineering from CSS, Accessibility to building micro frontend frameworks.`,
   },
   avatar_url: "/speakers/orr-gottlieb.png",
   contact: {
