8d55303e44f95234517d95e6472a4e5c4a22a1f7
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-20 20:34:20 +0100

    refactor(agendaItemModalContent): remove redundant code, add accessible colors

--- src/components/shared/AgendaItemModalContent.svelte
+++ src/components/shared/AgendaItemModalContent.svelte
@@ -1,6 +1,4 @@
 <script>
-    import SpeakerTitle from '../shared/SpeakerTitle.svelte';
-
     export let item = {};
     export let index = -1;
 
@@ -31,7 +29,7 @@
   background: #555;
 }
     .agenda-card {
-        margin: 40px auto;
+        margin: 20px auto;
         width: 100%;
         text-align: left;
         max-height: 350px;
@@ -40,21 +38,8 @@
         display: flex;
     }
 
-    .fix-height {
-        height: 350px !important;
-    }
-
-    .agenda-details h2 {
-        padding: 20px 0;
-        margin: 0;
-    }
-
-    .speaker-avatar {
-        max-width: 204px;
-    }
-
     .agenda-content {
-        padding-top: 40px;
+        padding-top: 10px;
     }
 
 
@@ -73,145 +58,28 @@
             max-width: 65%;
         }
     }
-
-    .agenda-content h4.speaker-type {
-        margin: 0;
-    }
-
-    .agenda-content h2.speaker-name {
-        color: var(--secondary-color);
-    }
-
-    .agenda-content h2.non-talk-title {
-        margin: 0;
-        padding: 0;
-    }
-
-    .agenda-content p.speaker-job-position {
-        margin: 0;
-        font-size: 13px;
-        font-weight: lighter;
-    }
-
     .agenda-content h4.talk-title {
-        color: var(--secondary-color);
+        color: var(--primary-color);
         text-transform: initial;
+        font-size: 21px;
     }
 
     .agenda-content p.talk-abstract {
         font-weight: lighter;
         margin-top: 0;
         white-space: pre-line;
+        color: black;
     }
 
-    .iterator {
-        border-bottom: 2px solid var(--snow-color);
-        text-align: left;
-        display: block;
-        font-size: 13px;
-    }
-
-    .tags {
-        padding: 20px 0;
-        display: block;
-    }
-
-    .tag {
-        color: var(--snow-color);
-        margin: 0 20px 0 0;
-        font-weight: bold;
-    }
-
-    /* for keynote */
-
-    .keynote .agenda-content h2.speaker-name {
-        color: var(--primary-color);
-    }
-
-    .keynote .agenda-content h4.talk-title {
-        color: var(--primary-color);
-    }
-
-    /* for lightning talk */
-
-    .lightning-talk .agenda-content h2.speaker-name {
-        color: var(--mix-color);
-    }
-
-    .lightning-talk .agenda-content h4.talk-title {
-        color: var(--mix-color);
-    }
-
-    .scrollbarOff {
-        overflow: hidden !important;
-    }
-
-    @media (max-width: 960px) {
-        .speaker-avatar {
-            max-width: 100%;
-        }
-    }
-
-    @media (max-width: 480px) {
-        .tags {
-            display: grid;
-        }
-    }
 </style>
-<div class="agenda-card { item.type } { getClass() }" class:scrollbarOff={!item.type || item.start === "16:00"} class:fix-height={item.type === 'talk' && item.details.speaker.name === 'Yonatan Kra'}>
-    <div class="d-none agenda-details mx-2">
-        {#if item.type === 'talk' }
-            <a
-                href="/speakers"
-                tabindex="-1"
-            >
-                <img
-                    src="{ item.details.speaker.avatar_url }"
-                    class="speaker-avatar"
-                    alt="{ item.details.speaker.name }"
-                />
-            </a>
-        {/if}
-    </div>
+<div class="agenda-card { item.type } { getClass() }">
     <div class="agenda-content col-lg-9 col-xs-12">
-        {#if item.type === 'talk' }
-            <h4 class="d-none speaker-type">
-                <SpeakerTitle talk={item.details}/>
-            </h4>
-            <h2 class="d-none speaker-name m-0 p-0">
-                { item.details.speaker.name }
-            </h2>
-            <p class="d-none speaker-job-position">
-                { item.details.speaker.position }
+  
+     <h4 class="talk-title mt-0 p-0 mb-3">
+        { item.details.title }
+     </h4>
 
-                {#if item.details.speaker.company}
-                    @ { item.details.speaker.company }
-                {/if}
-            </p>
-
-            <h4 class="talk-title m-0 p-0">
-                { item.details.title }
-            </h4>
-
-            <p class="talk-abstract d-sm-block">{ item.details.abstract.trim() }</p>
-
-            {#if item.details.slides_url}
-                <p>
-                    <a
-                        href="{ item.details.slides_url }"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >Slides</a>
-                </p>
-            {/if}
-        {/if}
-
-        {#if item.type === 'talk-not-ready' }
-            <h2 class="not-ready-speaker-name">{ item.details }</h2>
-        {/if}
-
-        {#if item.type === '' }
-            <h2 class="non-talk-title">{ item.details }</h2>
-        {/if}
+     <p class="talk-abstract d-sm-block">{ item.details.abstract.trim() }</p>
+   
     </div>
 </div>
