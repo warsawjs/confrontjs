4e03861fe272104e4840cf9dab5cf48f050eeeea
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-23 09:39:10 +0100

    refactor(talks): update Avitaild talk title and abstract

--- src/data/talks.js
+++ src/data/talks.js
@@ -1,9 +1,10 @@
 module.exports = [
   {
-    title: "Design Systems: The journey from chaos to consistency",
-    abstract: `Design systems are everywhere. At some point, we’ve all used bootstrap, material, or shoelace.  But we end up tweaking and changing and struggling with out of the box solutions, until we begin to fantasize about creating our own design system. 
-    But what does that process look like? What happens behind the scenes? Do we get enough value to justify the cost? 
-    This talk will describe the journey we took at Vonage to create our own design system, the challenges we faced on the way, and the surprising insights that turned an internally used tool into a customer facing product.`,
+    title:
+      "You Are The Storyteller: What fairytales can teach us about front end development",
+    abstract: `What do grimms’ fairy tales and writing code have in common? A lot.
+                Our world is built on stories, and so too the engineering world. Behind each standard, best practice, and conference talk, lies a story.
+                It’s up to us to see those stories, learn from them, and write future stories for generations of developers to come.`,
     tech: ["JavaScript", "Node.js", "Web Standards"],
     speaker: require("./speakers/avital-tzubeli"),
     slides_url: "",
